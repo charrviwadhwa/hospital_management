a17d14bac96192125e2a23a0df89797d0eea6a84
charrviwadhwa <charviwadhwa06@gmail.com>
2024-12-04 19:23:06 +0530

    updaated

--- hospital_management/hospital_management/settings.py
+++ hospital_management/hospital_management/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fr70@13wv*7z&l00rkw8axk-6_++o*xyu+%s19=7#(!cp%4b7$'
+#SECRET_KEY = 'django-insecure-fr70@13wv*7z&l00rkw8axk-6_++o*xyu+%s19=7#(!cp%4b7$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = ['hospital-management-bice.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
